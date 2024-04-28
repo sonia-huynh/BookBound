ab3ad23aeb6bdb9ed614ab411aa63e72359aaf92
Sonia Huynh <soniahuynhkl@gmail.com>
2024-04-29 11:51:13 +1200

    truncate book detail description to 3 sentences

--- client/components/Pages/SearchResults.tsx
+++ client/components/Pages/SearchResults.tsx
@@ -7,6 +7,17 @@ export default function SearchResults() {
     searchParams.get('q') || '',
   )
 
+  function shorten(description: string) {
+    const maxDescriptionLength = 3
+    const sentence = description.split('.')
+    if (description.length <= maxDescriptionLength) {
+      return description
+    } else {
+      const shorterSentence = sentence.slice(0, maxDescriptionLength)
+      return shorterSentence.join('.') + '...'
+    }
+  }
+
   if (isPending) {
     return <p>Searching...</p>
   }
@@ -20,21 +31,31 @@ export default function SearchResults() {
       <div className="flex justify-center	">
         <h1 className="text-3xl font-bold underline	">Search Results</h1>
       </div>
-      <div className="flex justify-center	">
+      <br></br>
+      <div className="flex justify-center">
         {data && (
-          <div className="flex max-w-sm	justify-center rounded-lg border border-gray-200 bg-white p-6">
-            <ol>
-              {data.map((details) => (
-                <li key={details.image}>
-                  <h3>{details.title}</h3>
-                  <h4>by {details.author}</h4>
+          <div className="container">
+            {data.map((details) => (
+              <div
+                key={details.image}
+                className="book-details flex flex-col rounded-lg border border-gray-200 bg-white p-6"
+              >
+                <div className="detail">
                   <img
                     src={details.image}
                     alt={`cover of book ${details.title}`}
+                    className="mt-4"
                   />
-                </li>
-              ))}
-            </ol>
+                  <p className="mb-2">{details.title}</p>
+                  <p className="mb-2">by {details.author}</p>
+                </div>
+                <div className="">
+                  <p className="mb-4 text-base">
+                    {shorten(String(details.description))}
+                  </p>
+                </div>
+              </div>
+            ))}
           </div>
         )}
       </div>
