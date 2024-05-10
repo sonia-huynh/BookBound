f9b5c9b9ccc8d7f887c48992cb4e929704ee6839
Sonia Huynh <soniahuynhkl@gmail.com>
2024-05-10 13:32:53 +1200

    style mybooks

--- client/components/Pages/MyBooks.tsx
+++ client/components/Pages/MyBooks.tsx
@@ -1,3 +1,4 @@
+import '../../styles/myBooks.css'
 import { useGetBooks } from '../../hooks/useBooks'
 
 export default function MyBooks() {
@@ -16,13 +17,13 @@ export default function MyBooks() {
       <div>
         <h1 className="ml-10 mt-10 text-3xl font-bold underline">My Books</h1>
         <br></br>
-        <div className="ml-10 flex space-x-8">
+        <div className="bookshelfContainer">
           {data.map((book) => (
             <div key={book.id}>
               <img
                 src={book.image}
                 alt={`cover of book ${book.title}`}
-                className="book-cover mt-4"
+                className="book-cover m-2"
               />
             </div>
           ))}
