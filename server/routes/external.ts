33fe3d4a355f48a292b961933becc3d2895354de
Sonia Huynh <soniahuynhkl@gmail.com>
2024-05-17 13:17:31 +1200

    create new get request for specific book by id

--- server/routes/external.ts
+++ server/routes/external.ts
@@ -42,6 +42,39 @@ router.get('/search', async (req, res) => {
     res.json(bookDetailsArray)
   } catch (error) {
     console.log(error)
-    res.status(500).json({ message: 'Something went wrong' })
+    res.status(500).json({ message: 'Something went wrong in finding books' })
+  }
+})
+
+// get book by searched book id
+router.get('/search/:id', async (req, res) => {
+  try {
+    const id = req.params.id
+    const bookResponse = await request.get(
+      `https://www.googleapis.com/books/v1/volumes/${id}`,
+    )
+
+    const details = bookResponse.body
+    if (!details) {
+      return res.status(404).json({ message: 'Book by id not found' })
+    }
+    const bookDetailsArray = [] // Array to store book details
+
+    const bookId = details.id
+    const volumeInfo = details.volumeInfo
+    const title = volumeInfo.title
+    const author = volumeInfo.authors[0]
+    const description = volumeInfo.description
+    const rating = volumeInfo.averageRating
+    const image = volumeInfo.imageLinks?.thumbnail + '&fife=w300'
+    const bookDetails = { bookId, title, author, description, rating, image } // Object containing book details
+    bookDetailsArray.push(bookDetails) // Push book details to array
+
+    res.json(bookDetailsArray)
+  } catch (error) {
+    console.log(error)
+    res
+      .status(500)
+      .json({ message: 'Something went wrong in trying to find book by id' })
   }
 })
