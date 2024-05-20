c350b4e9f818fe5f6a094da0dfc70accb89da45f
Sonia Huynh <soniahuynhkl@gmail.com>
2024-05-20 20:35:28 +1200

    create update review router

--- server/routes/books.ts
+++ server/routes/books.ts
@@ -1,7 +1,7 @@
-import checkJwt, { JwtRequest } from '../auth0.ts'
-import { StatusCodes } from 'http-status-codes'
+// import checkJwt, { JwtRequest } from '../auth0.ts'
+// import { StatusCodes } from 'http-status-codes'
 import express from 'express'
-import request from 'superagent'
+// import request from 'superagent'
 // import 'dotenv/config'
 
 import * as db from '../db/books.ts'
@@ -10,8 +10,6 @@ const router = express.Router()
 
 router.post('/', async (req, res) => {
   try {
-    // const title = req.query.q
-    // const author = req.body
     const details = req.body
     await db.addBook(details)
   } catch (error) {
@@ -43,13 +41,16 @@ router.get('/:title', async (req, res) => {
 })
 
 // this can add and update the book review since review already exists:
-router.patch('/:title', async (req, res) => {
+router.patch('/:id', async (req, res) => {
   try {
-    const title = req.params.title
-    const update = req.body
-    await db.updateReview(title, update)
-
-    return res.status(200).json({ message: 'Review updated' })
+    const bookId = String(req.query.id)
+    const bookReview = req.body.review
+    const result = await db.updateReview(bookId, bookReview)
+    if (result) {
+      res.json({ message: 'Review updated successfully', review: bookReview })
+    } else {
+      res.status(404).json({ message: 'Book not found' })
+    }
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: 'Something went wrong' })
