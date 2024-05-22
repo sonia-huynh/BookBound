e60e552aad1bf21840a8e426532da7bca5552057
Sonia Huynh <soniahuynhkl@gmail.com>
2024-05-22 22:44:48 +1200

    add all review hooks to one file

--- client/hooks/useGetReview.ts
+++ client/hooks/useGetReview.ts
@@ -1,5 +1,10 @@
-import { useQuery } from '@tanstack/react-query'
-import { getReviewById } from '../apis/books'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import {
+  addReview,
+  deleteReview,
+  getReviewById,
+  updateReview,
+} from '../apis/books.ts'
 
 export function useGetReviewById(bookId: string) {
   return useQuery({
@@ -11,6 +16,52 @@ export function useGetReviewById(bookId: string) {
         throw new Error('Failed to fetch book review')
       }
       return data
+    },
+  })
+}
+
+export function useAddReview() {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: (reviewDetails: {
+      bookId: string
+      title: string
+      review: string
+    }) =>
+      addReview(
+        reviewDetails.bookId,
+        reviewDetails.title,
+        reviewDetails.review,
+      ),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['review'],
+      })
+    },
+  })
+}
+
+export function useDeleteReview() {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: deleteReview,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['review'],
+      })
+    },
+  })
+}
+
+export function useUpdateReview() {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: (update: { bookId: string; review: string }) =>
+      updateReview(update.bookId, update.review),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['review'],
+      })
     },
   })
 }
